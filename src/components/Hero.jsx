a1514aa15eb514810374ea0922be0471992b31ae
Sanjay Kothari <{kotharikalash05@gmail.com}>
2025-09-12 15:03:11 +0530

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,18 +3,23 @@ import React from 'react'
 const Hero = () => {
   return (
     <div className="flex items-center justify-center pt-44">
-        <div className="flex flex-col items-center">
-          <p className="text-black/70 text-lg">A new browser from <u>Perplexity</u></p>
-          <h1 className="text-8xl font-thin  tracking-tighter">A personal AI assistant</h1>
+      <div className="flex flex-col items-center">
+        <p className="text-black/70 text-lg">A new browser from <u>Perplexity</u></p>
+        <h1 className="text-8xl font-sans  tracking-tighter">A personal AI assistant</h1>
 
-          <div className="rounded-full bg-black px-12 py-4 mt-8 text-white flex justify-between items-center">
-             <div className="rounded-full ">
-                <img src="#" alt="#" />
-             </div>
-            <p>Get Conenct</p>
+        <div className="rounded-full bg-black px-12 py-4 mt-8 text-white flex justify-between items-center">
+          <div className="rounded-full ">
+            <img src="#" alt="#" />
           </div>
+          <p>Get Conenct</p>
         </div>
+        {/* use of custom font div */}
+        <div className="mt-8 text-black/70 text-lg font-myfont italic">
+          <p>Powered by GPT-4, DALL·E 3, and more</p>
+        </div>
+
       </div>
+    </div>
   )
 }
 
